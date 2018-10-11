1036cb7bb59c3588a4ed78a02a97090c97246bea
Skyla Moore <s.moore5104@gmail.com>
2018-10-11 11:00:39 +0100

    First upload, major confusion

---
+++ ex9.py
@@ -0,0 +1,15 @@
+semi_annual_raise = 0.07
+return_rate = 0.04
+down_payment = 0.25
+cost_of_house = 1000000
+epsilon = 100
+num_searches = 0
+
+low = 0
+high = cost_of_house
+
+guess = (high + low)/2.0
+
+starting_salary = int(Input("Enter starting salary: "))
+
+#current_savings = 
