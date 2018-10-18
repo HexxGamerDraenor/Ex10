87690b4531724b348d9b0912ed62fbb43055dd5f
Skyla Moore <s.moore5104@gmail.com>
2018-10-18 11:05:49 +0100

    Correct file, mod comparison added

--- ex10.py
+++ ex10.py
@@ -4,11 +4,11 @@ high = int(input("Input high value "))
 print(low)
 while(low < high):
     low = low + 1
-    if(low%3 and low%5):
+    if(low%3 == 0 and low%5 == 0):
         print ("FizzBuzz")
-    elif(low%5):
+    elif(low%5 == 0):
         print("Buzz")
-    elif (low%3):
+    elif (low%3 == 0):
         print("Fizz")
     else:
         print(low)
