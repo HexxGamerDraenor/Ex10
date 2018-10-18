004a75ab65f4e836be3a6ed85cf2b554cf9d7927
Skyla Moore <s.moore5104@gmail.com>
2018-10-18 11:01:57 +0100

    Attempt at FizzBuzz, Fizz and Buzz working, combination error

--- ex10.py
+++ ex10.py
@@ -4,4 +4,11 @@ high = int(input("Input high value "))
 print(low)
 while(low < high):
     low = low + 1
-    print(low)
+    if(low%3 and low%5):
+        print ("FizzBuzz")
+    elif(low%5):
+        print("Buzz")
+    elif (low%3):
+        print("Fizz")
+    else:
+        print(low)
